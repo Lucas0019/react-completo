dfe3870993242ce85e223f68a6bbdf670f07eace
Lucas <lxavier.00@outlook.com>
2021-05-08 00:23:13 -0300

    feat: :fire: Add component Events.js

---
+++ react-fundamentals/without-npm/Components/Events.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Produtos = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Camisa</button>
+      <button onClick={handleClick}>Bermuda</button>
+    </div>
+  );
+};
+
+
+export default Produtos
