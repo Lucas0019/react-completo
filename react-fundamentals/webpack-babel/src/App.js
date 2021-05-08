cab767342cccad90c307b9b49f2badc24d36b4a3
Lucas <lxavier.00@outlook.com>
2021-05-08 15:13:03 -0300

    feat: :fire: Add and Config webpack minimal

---
+++ react-fundamentals/webpack-babel/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return React.createElement(
+    'a',
+    { href: 'https://github.com/Lucas0019' },
+    'Github Lucas0019',
+  );
+};
+
+export default App;
