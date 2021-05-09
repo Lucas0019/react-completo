b034a5ab319e17a50dc85f491a54f79cb8c6aa2b
Lucas <lxavier.00@outlook.com>
2021-05-09 16:38:56 -0300

    feat: :fire: Add Hands On - JSX

---
+++ reactapp/src/Components/JSX/HandsOn.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const caixaAviso = {
+  background: "tomato",
+  padding: "12px",
+  color: "white",
+  borderRadius: "8px"
+}
+
+
+
+const HandsOn = () => {
+  const dados = mario;
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', ''))) 
+    .reduce((valorAnterior, valorAtual) => valorAnterior + valorAtual); // Somando os valores 
+
+  return (
+    <div>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativa'}
+        </span>
+      </p>
+      <p>Total: R$ {total}</p>
+      {total > 10000 && <p style={caixaAviso}>Você está gastando muito</p>}
+    </div>
+  );
+};
+
+export default HandsOn;
